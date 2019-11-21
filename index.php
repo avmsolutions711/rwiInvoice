a014cfcd136e44e205d3602e2481dfe09727452c
avmsolutions711 <55661990+avmsolutions711@users.noreply.github.com>
2019-11-21 11:20:33 -0600

    Update index.php

--- index.php
+++ index.php
@@ -45,7 +45,7 @@ if ($method == 'POST')
 		       $Status_MSG = "Your Cognizant ID $LanID has been validated. Thanks";
 
 		    // $URL="http://66.25.18.67:5555/rest/Default/new_restExp/_get?num1=12&num2=15";
-        $URL="https://b2bprod01.7-eleven.com:9002/rest/Default/new_restExp/_get?num1=12&num2=15";
+        $URL="https://b2bprod01.7-eleven.com:9002/rest/Default/new_rest/_get?num1=12&num2=15";
 
 $opts = array('http' =>
 
